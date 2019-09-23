b29e4796965a479bf9b253a92897c1dc247fb0d8
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-09-23 16:41:57 -0300

    add new use case for subtraction, division and multiplication

---
+++ app/src/main/java/br/com/cantinho/android_mvvm_clean_architecture/ui/calculator/usecases/AddictionUseCase.kt
@@ -0,0 +1,12 @@
+package br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.usecases
+
+import br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.OperationResult
+import br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.Success
+
+class AddictionUseCase {
+
+    fun performAddiction(paramA: Int, paramB: Int): OperationResult {
+        return Success( paramA + paramB )
+    }
+
+}
--- app/src/main/java/br/com/cantinho/android_mvvm_clean_architecture/ui/calculator/usecases/DivisionUseCase.kt
+++ app/src/main/java/br/com/cantinho/android_mvvm_clean_architecture/ui/calculator/usecases/DivisionUseCase.kt
@@ -3,10 +3,9 @@ package br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.usecases
 import br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.OperationResult
 import br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.Success
 
-class SumUseCase {
+class DivisionUseCase {
 
-    fun performSum(paramA: Int, paramB: Int): OperationResult {
-        return Success( paramA + paramB )
+    fun performDivision(paramA: Int, paramB: Int): OperationResult {
+        return Success( paramA / paramB )
     }
-
 }
