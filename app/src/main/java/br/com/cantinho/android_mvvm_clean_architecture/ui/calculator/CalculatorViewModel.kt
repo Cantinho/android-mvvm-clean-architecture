f90df1b7c7e92d338df65426336c0cb87e5f85cd
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-09-23 16:26:38 -0300

    add usecase for sum operation

--- app/src/main/java/br/com/cantinho/android_mvvm_clean_architecture/ui/calculator/CalculatorViewModel.kt
+++ app/src/main/java/br/com/cantinho/android_mvvm_clean_architecture/ui/calculator/CalculatorViewModel.kt
@@ -3,20 +3,19 @@ package br.com.cantinho.android_mvvm_clean_architecture.ui.calculator
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.cantinho.android_mvvm_clean_architecture.ui.calculator.usecases.SumUseCase
 
 class CalculatorViewModel: ViewModel() {
 
     private val _operationResult = MutableLiveData<OperationResult>()
     val operationResult: LiveData<OperationResult> = _operationResult
 
+    private val sumUseCase = SumUseCase()
+
     fun doAction(action: Action) {
         when(action){
-            is Sum -> sum(action)
+            is Sum -> _operationResult.value = sumUseCase.performSum( action.paramA, action.paramB )
         }
-    }
-
-    private fun sum(action: Sum) {
-        _operationResult.value = Success(action.paramA + action.paramB )
     }
 
 
